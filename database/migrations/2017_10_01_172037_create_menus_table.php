709b8f7efc11579bab152888f43551b56ebdfb56
GoranM <mi.goran@hotmail.com>
2017-10-02 11:16:45 +0200

    Menu

---
+++ database/migrations/2017_10_01_172037_create_menus_table.php
@@ -0,0 +1,22 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateMenusTable extends Migration {
+
+    public function up() {
+        Schema::create('menus', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->string('parent_id');
+            $table->timestamps();
+        });
+    }
+
+
+    public function down() {
+        Schema::drop("menus");
+    }
+}
