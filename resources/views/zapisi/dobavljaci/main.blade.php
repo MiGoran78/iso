33c483c8df0f37af0a7c0ce8b5a0f7d94381e38f
GoranM <mi.goran@hotmail.com>
2017-10-02 11:55:48 +0200

    Menu 1

--- resources/views/zapisi/dobavljaci/main.blade.php
+++ resources/views/zapisi/dobavljaci/main.blade.php
@@ -12,13 +12,20 @@
 
     <body style="height:100%">
         <div class="container" style="font-family: 'Verdana'; margin: 0px; margin-right: 20px; padding: 5px; padding-bottom: 0px; width: auto; height:100%" >
+
             <div class="panel panel-primary" style="margin: 0px">
                 <div class="panel-heading" style="background: #5b5b5b; color: #ffffff; border: solid 1px #3b3b3b">
-                    <h4 style="margin: 0px">
-                        ISO standard
-                    </h4>
+                    <div class="row">
+                        <div class="col-md-11" align="left">
+                            <h4 style="margin: 0px"> ISO standard </h4>
+                        </div>
+                        <div class="col-md-1" align="right">
+                            @include('menu')
+                        </div>
+                    </div>
                 </div>
             </div>
+
         </div>
 
         @yield('content')
