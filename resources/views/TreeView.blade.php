ee7cc8d5c26c734b77b4ceddb5012d545a764a0e
MiGoran78 <mi.goran@hotmail.com>
2017-12-26 20:35:58 +0100

    Auth - update

--- resources/views/TreeView.blade.php
+++ resources/views/TreeView.blade.php
@@ -2,13 +2,16 @@
 
 <html>
 <head>
-    <title>ISO 9001</title>
+    <title>{{ config('app.name', 'Laravel') }}</title>
     <link rel="stylesheet" href="/css/font-awesome.min.css" />
+    <link rel="stylesheet" href="/css/font-raleway.css" />
     <link rel="stylesheet" href="/css/bootstrap.min.css">
     <link rel="stylesheet" href="/css/css.css?family=Lato:100,300,400,700">
     <link rel="stylesheet" href="/css/treeview.css">
     <script src="/js/jquery.min.js"></script>
-    <link href="/css/app.css" rel="stylesheet">
+    {{--<link href="/css/app.css" rel="stylesheet">--}}
+
+    <apex:page showHeader='true' X-UA-Compatible="chrome-1">
 </head>
 
 <body style="height:100%">
@@ -18,15 +21,14 @@
 
 
 <div id="app">
-    <nav class="navbar navbar-default navbar-static-top">
-        <div class="container">
-
+    <nav class="navbar navbar-default navbar-static-top" style="margin: 0">
+        <div class="container" style="font-family: Raleway; margin-right: 40px">
 
             <ul class="nav navbar-nav navbar-right">
                 <!-- Authentication Links -->
                 @if (Auth::guest())
                     <li><a href="{{ url('/login') }}">Login</a></li>
-                    {{--<li><a href="{{ url('/register') }}">Register</a></li>--}}
+                    <li><a href="{{ url('/register') }}">Register</a></li>
                 @else
                     <li class="dropdown">
                         <a href="#" class="dropdown-toggle" data-toggle="dropdown" role="button" aria-expanded="false">
@@ -47,13 +49,15 @@
                     </li>
                 @endif
             </ul>
+
+
         </div>
     </nav>
 </div>
 
 
 
-<div class="container" style="font-family: 'Verdana'; margin: 5px; margin-right: 20px; padding: 0px; width: auto; height:100%">
+<div class="container" style="font-family: 'Verdana'; margin: 5px; margin-top: 0px; margin-right: 20px; padding: 0px; width: auto; height:100%">
 
     @include('header')
 
